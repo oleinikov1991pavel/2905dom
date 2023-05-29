dcfaa717316e8fcc14ee613df14b41a251961952
oleinikov1991pavel <134002094+oleinikov1991pavel@users.noreply.github.com>
2023-05-29 15:09:26 +0300

    123

---
+++ main.py
@@ -0,0 +1,16 @@
+#6.4
+count = 0
+
+while True:
+    n = float(input('Введите число\n'))
+    if n < 0:
+        count = count + 1
+    if n > 0:
+        break
+print(count)
+
+
+#6.6(б)
+#вопросы
+#6.42
+#вопросы
