169f1ca9ecf839ea01043e10edd3cab6d75f046d
oleinikov1991pavel <134002094+oleinikov1991pavel@users.noreply.github.com>
2023-06-15 16:36:45 +0300

    2555

---
+++ main.py
@@ -0,0 +1,61 @@
+#11.16
+# numbers = [10, 12, 55, 66, 13, 9]
+# s = numbers[2]
+# k = numbers[2]
+# c = (s < k)
+# if s > 0:
+#     print(True)
+# else:
+#     print(False)
+#
+# if k % 2 == 0:
+#     print(True)
+# else:
+#     print(False)
+#
+# if c == True:
+#     print('s больше')
+# else:
+#     print('k больше')
+#
+
+#11.18 (а,б,в)
+
+# numbers = [10, 12, 55, 66, 13, 9]
+#
+# index = 0
+# while index < len(numbers):
+#     numbers[index] -= 20
+#     index += 1
+# print(numbers)
+
+# while index < len(numbers):
+#     numbers[index] = numbers[index] * numbers[-1]
+#     index += 1
+# print(numbers)
+
+# b = numbers[0]
+#
+# while index < len(numbers):
+#     numbers[index] += b
+#     index += 1
+# print(numbers)
+
+
+#11.21
+
+# numbers = [10, 12, 55, 66, 13, 9]
+# print(sum(numbers))
+
+#11.30
+
+# numbers = []
+# if sum(numbers) > 999:
+#     print(True)
+# else:
+#     print(False)
+
+#11.36(a,в)
+#
+
+
